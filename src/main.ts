f8851c22164e654f727ebb60301cd686e5e44bba
Jeel Parikh <jeel.parikh@bacancy.com>
2024-07-15 14:32:51 +0530

    feat: add husky and lint-staged

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ENV_VARIABLES } from './config/env.config';
-import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  app.enableVersioning({
-    type: VersioningType.URI,
-  });
-  await app.listen(ENV_VARIABLES.PORT || 3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
+    app.enableVersioning({
+      type: VersioningType.URI,
+    });
+    const port = ENV_VARIABLES.PORT || 3000;
+    await app.listen(port);
+    Logger.log(`Listing on port: ${port}`, 'MAIN');
+  } catch (err) {
+    Logger.error(`Something went wrong: ${err.message}`);
+  }
 }
 bootstrap();
